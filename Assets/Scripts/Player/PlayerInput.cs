81a32a15f2fba0d84bb2f45af9b03814e99dc819
Joscha Götzer <joscha.goetzer@gmail.com>
2024-01-08 19:58:48 +0100

    Fix null ref exception in PlayerInput

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -6,11 +6,11 @@ namespace Player
     {
         public Vector2 MovementVector { get; private set; }
         public Vector2 LookDirectionVector { get; private set; }
-        public bool Jumping { get; private set;}
-        public bool Shooting { get; private set;}
+        public bool Jumping { get; private set; }
+        public bool Shooting { get; private set; }
 
-        public bool IsGamepad { get; private set;}
-        public int WeaponSelection { get; private set;}
+        public bool IsGamepad { get; private set; }
+        public int WeaponSelection { get; private set; }
         private CustomInput _input;
 
         private void Awake()
@@ -31,6 +31,8 @@ namespace Player
 
         private void HandleInputs()
         {
+            if (_input == null) return;
+
             MovementVector = _input.Player.Movement.ReadValue<Vector2>();
             LookDirectionVector = _input.Player.LookDirection.ReadValue<Vector2>();
             Jumping = _input.Player.Jumping.ReadValue<float>() != 0;
