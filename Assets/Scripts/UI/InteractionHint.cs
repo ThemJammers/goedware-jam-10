d444ea7e89bc6aa208aefca87fe91cd9fada1784
Joscha Götzer <joscha.goetzer@gmail.com>
2024-01-09 20:10:05 +0100

    Move InteractionHint to namespace

--- Assets/Scripts/UI/InteractionHint.cs
+++ Assets/Scripts/UI/InteractionHint.cs
@@ -1,34 +1,34 @@
-using System;
 using TMPro;
 using UnityEngine;
 
-public class InteractionHint : MonoBehaviour
+namespace UI
 {
-    private Animation fadeInAnimation;
-    private TextMeshPro tmp;
-    private SpriteRenderer backgroundSpriteRenderer;
-
-    private bool _isVisible;
-
-    public bool IsVisible => _isVisible;
-
-    public void Start()
+    public class InteractionHint : MonoBehaviour
     {
-        fadeInAnimation = GetComponent<Animation>();
-        gameObject.SetActive(false);
-    }
+        private Animation fadeInAnimation;
+        private TextMeshPro tmp;
+        private SpriteRenderer backgroundSpriteRenderer;
 
-    public bool Show()
-    {
-        gameObject.SetActive(true);
-        _isVisible = true;
-        fadeInAnimation.Play(PlayMode.StopAll);
-        return true;
-    }
+        public bool IsVisible { get; private set; }
 
-    public void Hide()
-    {
-        gameObject.SetActive(false);
-        _isVisible = false;
+        public void Start()
+        {
+            fadeInAnimation = GetComponent<Animation>();
+            gameObject.SetActive(false);
+        }
+
+        public bool Show()
+        {
+            gameObject.SetActive(true);
+            IsVisible = true;
+            fadeInAnimation.Play(PlayMode.StopAll);
+            return true;
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+            IsVisible = false;
+        }
     }
 }
